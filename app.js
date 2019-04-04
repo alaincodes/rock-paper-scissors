fc0187535f748f271be35b2b7d98f6a07569d2b1
Alain Tran <alaint77@gmail.com>
2019-04-04 14:39:46 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -1,30 +1,115 @@
 // ROCK PAPER SCISSORS THE ODIN PROJECT
+const p_rock = document.getElementById('p_rock');
+const p_paper = document.getElementById('p_paper');
+const p_scissors = document.getElementById('p_scissors')
 
-let computerPlay = () => {
-  const choices = ["rock", "paper", "scissors"];
-  let result = Math.floor(Math.random() * 3);
-  return choices[result];
+const c_rock = document.getElementById('c_rock');
+const c_paper = document.getElementById('c_paper');
+const c_scissors = document.getElementById('c_scissors');
+
+const p_score = document.getElementById('p_score');
+const c_score = document.getElementById('c_score');
+
+const resetBtn = document.getElementById('resetBtn');
+
+let player_choice = '';
+let computer_choice = '';
+
+let player_score = 0;
+let computer_score = 0;
+
+function win() {
+  player_score++;
+  p_score.innerHTML = player_score;
+  c_score.innerHTML = computer_score;
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) { return "IT'S A TIE" } 
+function lose() {
+  computer_score++;
+  p_score.innerHTML = player_score;
+  c_score.innerHTML = computer_score;
+}
 
-  if (playerSelection === "rock") {
-    if (computerSelection === "scissors") {
-      return "YOU WIN CPU LOSE!"
-    } else { return "CPU WIN YOU LOSE!" }
+function draw() {
+  p_score.innerHTML = player_score;
+  c_score.innerHTML = computer_score;
+}
+
+p_rock.addEventListener('click', () => {
+  player_choice = 'rock';
+  p_rock.setAttribute('style', "background: green;");
+  computerPlay();
+  playRound(player_choice, computer_choice);
+});
+
+p_paper.addEventListener('click', () => {
+  player_choice = 'paper';
+  p_paper.setAttribute('style', "background: green;");
+  computerPlay();
+  playRound(player_choice, computer_choice);
+});
+
+p_scissors.addEventListener('click', () => {
+  player_choice = 'scissors';
+  p_scissors.setAttribute('style', "background: green;");
+  computerPlay();
+  playRound(player_choice, computer_choice);
+});
+
+// Random number between 0, 1 or 2 (Rock, Paper or Scissors)
+let computerPlay = () => {
+  const comp_choice = Math.floor(Math.random() * 3);
+
+  if (comp_choice === 0) {
+    c_rock.setAttribute('style', "background: green;");
+    computer_choice = "rock";
+  } else if (comp_choice === 1) {
+    c_paper.setAttribute('style', "background: green");
+    computer_choice = "paper";
+  } else {
+    c_scissors.setAttribute('style', "background: green");
+    computer_choice = "scissors";
+  }
+}
+
+
+function playRound(player_choice, computer_choice) {
+  if (player_choice === computer_choice) { 
+    draw();
+   } 
+
+  if (player_choice === "rock") {
+    if (computer_choice === "scissors") {
+      win();
+    } else { lose(); }
   }
 
-  if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      return "YOU LOSE CPU WIN!"
-    } else { return "PLAYER WIN CPU LOSE!" }
+  if (player_choice === "scissors") {
+    if (computer_choice === "rock") {
+      lose();
+    } else { win(); }
   }
 
-  if (playerSelection === "paper") {
-    if (computerSelection === "rock") { 
-      return "CPU WIN YOU LOSE!"
-    } else { return "PLAYER WIN CPU LOSE!" }
+  if (player_choice === "paper") {
+    if (computer_choice === "rock") { 
+      lose();
+    } else { win(); }
   }
-
 };
+
+resetBtn.addEventListener('click', () => {
+  playAgain(player_choice, computer_choice);
+})
+
+function playAgain() {
+  p_rock.removeAttribute('style', "background: green;");
+  p_paper.removeAttribute('style', "background: green;");
+  p_scissors.removeAttribute('style', "background: green;");
+
+  c_rock.removeAttribute('style', "background: green;");
+  c_paper.removeAttribute('style', "background: green;");
+  c_scissors.removeAttribute('style', "background: green;");
+
+  p_score.innerHTML = 0;
+  c_score.innerHTML = 0;
+}
